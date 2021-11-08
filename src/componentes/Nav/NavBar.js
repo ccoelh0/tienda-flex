4af52456fc04e9e3cb43b20eaf2ac347b822c1b4
Agustin Coelho <77288330+ccoelh0@users.noreply.github.com>
2021-11-08 18:42:10 -0300

    Update NavBar.js

--- src/componentes/Nav/NavBar.js
+++ src/componentes/Nav/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import "../../componentes-css/Navbar/nav.css";
+import "../../componentes-css/navbar/nav.css";
 
 //Import components
 import CartWidget from "./CartWidget";
